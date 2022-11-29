1fea9065bcd54c111a7708e0177391670e2fd859
irvan taufik <irvantaufik28@gmail.com>
2022-11-29 23:00:17 +0700

    commit

---
+++ binarMockTest.js
@@ -0,0 +1,25 @@
+
+/*
+ * Complete the 'fizzBuzz' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function fizzBuzz(n) {
+   for (let i = 1; i <= n; i++) {
+       if (i % 3 == 0 && i % 5 == 0) {
+           console.log("FizzBuzz")
+       } else if (i % 3 == 0) {
+           console.log("Fizz") 
+       } else if (i % 5 == 0) {
+           console.log("Buzz")
+       } else 
+       console.log(i)
+   }
+
+}
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    fizzBuzz(n);
+}
